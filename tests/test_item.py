b6c653b7066766eb25f7b0b849247b03de3359f8
avdeevaa <tess-avdeeva@seznam.cz>
2023-09-05 11:14:50 +0200

    first commit + push: class item finished tests for item - 100% some changes in gitingore and pyproject

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount():
+    item = Item("Смартфон", 10000, 20)
+    item.pay_rate = 0.8
+    assert item.apply_discount() == 8000.0
