c209057ca7ff3c863d675796ced4cd1a75680303
avdeevaa <tess-avdeeva@seznam.cz>
2023-09-08 08:10:16 +0200

    second commit + push: class item new methods tests for new methods in item - 100% some changes in gitingore and pyproject

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,9 +1,10 @@
 from src.item import Item
+# import os
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
 
-    # длина наименования товара меньше 10 символов
+    #длина наименования товара меньше 10 символов
     item.name = 'Смартфон'
     assert item.name == 'Смартфон'
 
@@ -11,11 +12,17 @@ if __name__ == '__main__':
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
-    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    #current_dir = os.getcwd()
+    #file_path = os.path.join(current_dir, 'items.csv') # без этого и с отсутствием файла в папке homework не получалось
+
+    Item.instantiate_from_csv('items.csv')  # создание объектов из данных файла
+
+    # так как у меня получилось len(Item.all) == 6 +Суперсмарт
+    assert len(Item.all) == 6  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    # assert item1.name == 'Ńěŕđňôîí'
+    # assert item1.name == 'Суперсмарт'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,20 @@ def test_apply_discount():
     item = Item("Смартфон", 10000, 20)
     item.pay_rate = 0.8
     assert item.apply_discount() == 8000.0
+
+def test_name():
+    item = Item("Смартфон", 10000, 20)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    item.name = 'СуперСмартфон'
+    assert item.name == 'СуперСмарт'
+
+def test_instantiate_from_csv():
+    file_name = 'src/items.csv'
+    Item.instantiate_from_csv(file_name)
+    assert len(Item.all) == 8 # потому что он собирает и те предыдущие экземпляры(3 + 5 из файла)
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('60') == 60
+    assert Item.string_to_number('8.1') == 8
